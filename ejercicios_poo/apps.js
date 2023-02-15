2662fdaf4f7f12620b51e1212934f193801f2775
joelcampoverde <118577957+joelcampoverde@users.noreply.github.com>
2023-02-14 22:27:19 -0500

    Add files via upload

---
+++ ejercicios_poo/apps.js
@@ -0,0 +1,16 @@
+class Persona {constructor(nombre,cedula){
+    this.nombre=nombre;
+    this.cedula=cedula;
+}
+}
+
+class  Estudiante extends Persona{ constructor(nombre,cedula){
+    this.matricula=matricula;
+    this.apellido=apellido;
+    this.cedula=cedula;
+}
+}
+class Caso_Uso_estudiante extends Estudiante{ constructor(){
+this.Estudiante=Estudiante
+}
+}
